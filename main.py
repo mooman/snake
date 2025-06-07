13a6df40eed2a583a058ef81317bfbe20882f7a5
Rachot Moragraan <rachot@hey.com>
2025-06-06 22:10:48 -0700

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,13 @@
+screenWidth = scene.screen_width()
+screenHeight = scene.screen_height()
+gridSize = 8
+
+snake = []
+
+snakeSprite = sprites.create(assets.image('cat'))
+
+def gameUpdate():
+    pass
+
+game.on_update_interval(500, gameUpdate)
+
---
+++ main.ts
@@ -0,0 +1,8 @@
+let screenWidth = scene.screenWidth()
+let screenHeight = scene.screenHeight()
+let gridSize = 8
+let snake = []
+let snakeSprite = sprites.create(assets.image`cat`)
+game.onUpdateInterval(500, function gameUpdate() {
+    
+})
