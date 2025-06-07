a97b975e30d5f08171f8d1638bb1f4763833789d
Rachot Moragraan <rachot@hey.com>
2025-06-06 23:44:23 -0700

    Snake moves!

--- main.py
+++ main.py
@@ -2,12 +2,44 @@ screenWidth = scene.screen_width()
 screenHeight = scene.screen_height()
 gridSize = 8
 
-snake = []
+snake: List[List[number]] = [[10, 7], [10, 8], [10,9]]
+direction: List[number] = [0, -1]
 
-snakeSprite = sprites.create(assets.image('cat'))
+controller.any_button.on_event(ControllerButtonEvent.PRESSED, on_button_pressed)
 
-def gameUpdate():
-    pass
+def initialize():
+    game.on_update_interval(300, gameLoop)
+    
+def drawScreen():
+    sprites.destroy_all_sprites_of_kind(SpriteKind.player)
+    for segment in snake:
+        x = segment[0] * gridSize
+        y = segment[1] * gridSize
+        snakeSprite = sprites.create(assets.image('snakeSegment'), SpriteKind.player)
+        snakeSprite.set_position(x, y)
 
-game.on_update_interval(500, gameUpdate)
+def moveSnake():
+    global snake
+    head = snake[0]
+    newHead = [head[0] + direction[0], head[1] + direction[1]]
+    
+    snake.unshift(newHead)
+    snake.pop()
 
+def on_button_pressed():
+    global direction
+
+    if controller.left.is_pressed():
+        direction = [-1,0]
+    elif controller.right.is_pressed():
+        direction = [1, 0]
+    elif controller.up.is_pressed():
+        direction = [0, -1]
+    elif controller.down.is_pressed():
+        direction = [0, 1]
+
+def gameLoop():
+    moveSnake()
+    drawScreen()
+
+initialize()
--- main.ts
+++ main.ts
@@ -1,8 +1,47 @@
 let screenWidth = scene.screenWidth()
 let screenHeight = scene.screenHeight()
 let gridSize = 8
-let snake = []
-let snakeSprite = sprites.create(assets.image`cat`)
-game.onUpdateInterval(500, function gameUpdate() {
+let snake = [[10, 7], [10, 8], [10, 9]]
+let direction = [0, -1]
+controller.anyButton.onEvent(ControllerButtonEvent.Pressed, function on_button_pressed() {
+    
+    if (controller.left.isPressed()) {
+        direction = [-1, 0]
+    } else if (controller.right.isPressed()) {
+        direction = [1, 0]
+    } else if (controller.up.isPressed()) {
+        direction = [0, -1]
+    } else if (controller.down.isPressed()) {
+        direction = [0, 1]
+    }
     
 })
+function initialize() {
+    game.onUpdateInterval(300, function gameLoop() {
+        moveSnake()
+        drawScreen()
+    })
+}
+
+function drawScreen() {
+    let x: number;
+    let y: number;
+    let snakeSprite: Sprite;
+    sprites.destroyAllSpritesOfKind(SpriteKind.Player)
+    for (let segment of snake) {
+        x = segment[0] * gridSize
+        y = segment[1] * gridSize
+        snakeSprite = sprites.create(assets.image`snakeSegment`, SpriteKind.Player)
+        snakeSprite.setPosition(x, y)
+    }
+}
+
+function moveSnake() {
+    
+    let head = snake[0]
+    let newHead = [head[0] + direction[0], head[1] + direction[1]]
+    snake.unshift(newHead)
+    snake.pop()
+}
+
+initialize()
