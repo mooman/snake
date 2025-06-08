1f81d96cf304005f5e14efa7a19a328c6b5893fa
Rachot Moragraan <rachot@hey.com>
2025-06-08 15:48:01 -0700

    Functional complete

--- main.py
+++ main.py
@@ -4,13 +4,25 @@ gridSize = 8
 
 snake: List[List[number]] = [[10, 7], [10, 8], [10,9]]
 dx = 0
-dy = 1
+dy = -1
+
+food: List[number] = [0, 0]
 
 controller.any_button.on_event(ControllerButtonEvent.PRESSED, on_button_pressed)
 
 def initialize():
+    difficulty = game.ask_for_number('Difficulty 1,2,3', 1, True)
+    
+    if difficulty == 1:
+        difficulty = 300
+    elif difficulty == 2:
+        difficulty = 100
+    elif difficulty == 3:
+        difficulty = 50
+
+    info.player1.set_score(0)
     newFood()
-    game.on_update_interval(300, gameLoop)
+    game.on_update_interval(difficulty, gameLoop)
     
 def drawScreen():
     sprites.destroy_all_sprites_of_kind(SpriteKind.player)
@@ -19,12 +31,29 @@ def drawScreen():
         y = (segment[1] * gridSize) - gridSize/2
         snakeSprite = sprites.create(assets.image('snakeSegment'), SpriteKind.player)
         snakeSprite.set_position(x, y)
-
+    
+    sprites.destroy_all_sprites_of_kind(SpriteKind.food)
+    foodSprite = sprites.create(assets.image('food'), SpriteKind.food)
+    fx = (food[0] * gridSize) - (gridSize / 2)
+    fy = (food[1] * gridSize) - (gridSize / 2)
+    foodSprite.set_position(fx, fy)
+    
 def newFood():
-    pass
+    global food
+    insideSnake = True
+    while insideSnake:
+        foodX = randint(1, (screenWidth / gridSize) - 1)
+        foodY = randint(1, (screenHeight / gridSize) - 1)
+        insideSnake = False
+
+        for segment in snake:
+            if foodX == segment[0] and foodY == segment[1]:
+                insideSnake = True
+
+    food = [foodX, foodY]
 
 def moveSnake():
-    global snake
+    global snake, food
     head = snake[0]
     newHead = [head[0] + dx, head[1] + dy]
 
@@ -33,9 +62,22 @@ def moveSnake():
         newHead[1] > (screenHeight / gridSize) or
         newHead[1] <= 0):
         game.over(False)
-    
+
+    insideSnake = False
+    for segment in snake:
+        if newHead[0] == segment[0] and newHead[1] == segment[1]:
+            insideSnake = True
+
+    if insideSnake:
+        game.over(False)
+
     snake.unshift(newHead)
-    snake.pop()
+            
+    if food[0] == newHead[0] and food[1] == newHead[1]:
+        newFood()
+        info.player1.change_score_by(1)
+    else:
+        snake.pop()
 
 def on_button_pressed():
     global dx,dy
--- main.ts
+++ main.ts
@@ -3,7 +3,8 @@ let screenHeight = scene.screenHeight()
 let gridSize = 8
 let snake = [[10, 7], [10, 8], [10, 9]]
 let dx = 0
-let dy = 1
+let dy = -1
+let food = [0, 0]
 controller.anyButton.onEvent(ControllerButtonEvent.Pressed, function on_button_pressed() {
     
     if (controller.left.isPressed() && dx != 1) {
@@ -22,8 +23,18 @@ controller.anyButton.onEvent(ControllerButtonEvent.Pressed, function on_button_p
     
 })
 function initialize() {
+    let difficulty = game.askForNumber("Difficulty 1,2,3", 1, true)
+    if (difficulty == 1) {
+        difficulty = 300
+    } else if (difficulty == 2) {
+        difficulty = 100
+    } else if (difficulty == 3) {
+        difficulty = 50
+    }
+    
+    info.player1.setScore(0)
     newFood()
-    game.onUpdateInterval(300, function gameLoop() {
+    game.onUpdateInterval(difficulty, function gameLoop() {
         moveSnake()
         drawScreen()
     })
@@ -40,10 +51,30 @@ function drawScreen() {
         snakeSprite = sprites.create(assets.image`snakeSegment`, SpriteKind.Player)
         snakeSprite.setPosition(x, y)
     }
+    sprites.destroyAllSpritesOfKind(SpriteKind.Food)
+    let foodSprite = sprites.create(assets.image`food`, SpriteKind.Food)
+    let fx = food[0] * gridSize - gridSize / 2
+    let fy = food[1] * gridSize - gridSize / 2
+    foodSprite.setPosition(fx, fy)
 }
 
 function newFood() {
+    let foodX: number;
+    let foodY: number;
     
+    let insideSnake = true
+    while (insideSnake) {
+        foodX = randint(1, screenWidth / gridSize - 1)
+        foodY = randint(1, screenHeight / gridSize - 1)
+        insideSnake = false
+        for (let segment of snake) {
+            if (foodX == segment[0] && foodY == segment[1]) {
+                insideSnake = true
+            }
+            
+        }
+    }
+    food = [foodX, foodY]
 }
 
 function moveSnake() {
@@ -54,8 +85,25 @@ function moveSnake() {
         game.over(false)
     }
     
+    let insideSnake = false
+    for (let segment of snake) {
+        if (newHead[0] == segment[0] && newHead[1] == segment[1]) {
+            insideSnake = true
+        }
+        
+    }
+    if (insideSnake) {
+        game.over(false)
+    }
+    
     snake.unshift(newHead)
-    snake.pop()
+    if (food[0] == newHead[0] && food[1] == newHead[1]) {
+        newFood()
+        info.player1.changeScoreBy(1)
+    } else {
+        snake.pop()
+    }
+    
 }
 
 initialize()
